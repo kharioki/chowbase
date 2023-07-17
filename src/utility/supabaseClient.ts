d591ff1d85ad86728e2d3423545b4b6f82bb78ba
Tony Kharioki <antolisah@gmail.com>
2023-07-17 20:46:46 +0300

    connect to supabase

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,8 +1,11 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+// const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_URL = "https://feanuyxkmgnifjtifdyi.supabase.co";
+// const SUPABASE_KEY =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlYW51eXhrbWduaWZqdGlmZHlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODkzMzkyNzMsImV4cCI6MjAwNDkxNTI3M30.Q8QUcbq8edahMsZmeStyiDQxO-QCtud3IFLlt_bVeL8";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
